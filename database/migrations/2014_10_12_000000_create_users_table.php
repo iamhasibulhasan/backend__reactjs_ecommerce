260b52bc47b5a5817b766135e376412125dbc44f
imhasibulhasan <hashibul218@gmail.com>
2022-04-08 00:01:46 +0600

    admin and user setup

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,8 +17,9 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('status')->default(1);
+            $table->string('type')->default(1);
             $table->rememberToken();
             $table->timestamps();
         });
